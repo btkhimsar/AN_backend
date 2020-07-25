88af0cbce929bce77e6f25e04fd8ce2cdbd92bbb
rajat.gupta1 <Rajat@1008>
2020-07-25 17:58:26 +0530

    adding dependencies and other installation files

--- venv/bin/test.py
+++ venv/bin/test.py
@@ -6,5 +6,5 @@ db = con.testdb
 
 my_coll = db.coll_name
 
-emp_rec = {'name':"emp_name", 'address':"emp_addr", 'id':"emp_id"}
+emp_rec = {'name': "emp_name", 'address': "emp_addr", 'id': "emp_id"}
 rec_id = my_coll.insert_one(emp_rec)
